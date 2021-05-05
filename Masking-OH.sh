7e186f6ce6dad9e78a8379087a15ca1d797f6d0d
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-05-05 08:56:01 +0530

    Update Masking-OH.sh

--- Masking-OH.sh
+++ Masking-OH.sh
@@ -37,15 +37,14 @@ url_checker $phish
 short=$(curl -s https://da.gd/s/?url=${phish})
 shorter=${short#https://}
 echo -e "\n\e[1;31;42m ### Masking Domain ###\e[0m"
-echo 'Domain to mask the Phishing URL (with http or https), ex https://google.com , http
-://anything.org) :'
+echo 'Domain to mask the Phishing URL \e[91m( Ex:\e[96m https://google.com , http://anything.org )\e[0m :'
 echo -en "\e[32m=>\e[0m "
 read mask
 url_checker $mask
-echo -e '\nType social engineering words:(like free-money, best-pubg-tricks)'
+echo -e '\nType social engineering words \e[91m(Ex:\e[96m like, free-money, best-pubg-tricks)'
 echo -e "\e[93mDon't use space just use '-' between social engineering words\e[0m"
 echo -en "\e[32m=>\e[0m "
 read words
-echo -e "\nGenerating Masking Online Hacking Link...\n"
+echo -e \e[95m"\nGenerating Masking Online Hacking Link...\n"
 final=$mask-$words@$shorter
 echo -e "Here is the Masking OH URL:\e[32m ${final} \e[0m\n"
