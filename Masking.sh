de5219fd74a23f84f1e0d17bb0001494caebb5d5
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-09-19 11:40:34 +0530

    Update Masking.sh

--- Masking.sh
+++ Masking.sh
@@ -1,4 +1,5 @@
-# Bash Script for Hide Phishing URL Created by Online Hacking SUMAN
+#!/bin/bash
+# Bash Script for Hide Phishing URL Created by Suman Online Hacking
 
 url_checker() {
     if [ ! "${1//:*}" = http ]; then
@@ -8,52 +9,58 @@ url_checker() {
         fi
     fi
 }
+
 echo ""
+clear
 echo ""
 echo -e " \e[92m   ╔═══════════════════════════════════════════════════════════╗ "
 echo -e " \e[91m   ║ \e[93m ███    ███  █████  ███████ ██   ██ ██ ███    ██  ██████  \e[91m║ "
 echo -e " \e[91m   ║ \e[93m ████  ████ ██   ██ ██      ██  ██  ██ ████   ██ ██       \e[91m║"
 echo -e " \e[91m   ║ \e[93m ██ ████ ██ ███████ ███████ █████   ██ ██ ██  ██ ██   ███ \e[91m║ "
 echo -e " \e[91m   ║ \e[93m ██  ██  ██ ██   ██      ██ ██  ██  ██ ██  ██ ██ ██    ██ \e[91m║ "
-echo -e " \e[91m   ║ \e[93m ██      ██ ██   ██ ███████ ██   ██ ██ ██   ████  ██████  \e[91m║ \e[33mSUMAN"
-echo -e " \e[96m   ╚═══════════════════════════════════════════════════════════╝  \e[33mV2.2 "
+echo -e " \e[91m   ║ \e[93m ██      ██ ██   ██ ███████ ██   ██ ██ ██   ████  ██████  \e[91m║ "
+echo -e " \e[96m   ╚═══════════════════════════════════════════════════════════╝  \e[0m "
+echo -e "                                                       \e[33mSUMAN \e[33mV2.2 "   
 echo ""
+echo -e " \e[1m          \e[1;91m [\e[1;96m*\e[1;91m] \e[1;97mWebsite  \e[1;91m= \e[5m \e[1;97mwww.termux.xyz  "
 echo ""
-echo -e " \e[1m          \e[1;91m [\e[1;96m*\e[1;91m] \e[1;97mYouTube  \e[1;91m= \e[5m \e[1;97mOnline Hacking  "
-echo ""
-echo -e " \e[5m         \e[25m \e[1;91m [\e[1;96m*\e[1;91m] \e[1;97mWebsite  \e[1;91m=  \e[4m\e[1;97mwww.OnlineHacking.xyz\e[24m   "
+echo -e " \e[5m         \e[25m \e[1;91m [\e[1;96m*\e[1;91m] \e[1;97mWebsite  \e[1;91m=  \e[4m\e[1;97mwww.OnlineHacking.in\e[24m   "
 echo ""
 echo -e " \e[1m          \e[1;91m [\e[1;96m*\e[1;91m] \e[1;97mTelegram \e[5m\e[1;91m=  \e[1;97m@OnlineHacking   \e[93m   "
 echo ""
 echo ""
-echo -e " \e[92m                    ╔═╗\e[95m┌┐┌┬  ┬┌┐┌┌─┐ \e[94m ╦ ╦\e[95m┌─┐┌─┐┬┌─┬┌┐┌┌─┐  "
-echo -e " \e[92m                    ║ ║\e[95m││││  ││││├┤  \e[94m ╠═╣\e[95m├─┤│  ├┴┐│││││ ┬  "
-echo -e " \e[92m                    ╚═╝\e[95m┘└┘┴─┘┴┘└┘└─┘ \e[94m ╩ ╩\e[95m┴ ┴└─┘┴ ┴┴┘└┘└─┘ \e[32m "
+echo -e " \e[92m                 ╔═╗\e[95m┌┐┌┬  ┬┌┐┌┌─┐ \e[94m ╦ ╦\e[95m┌─┐┌─┐┬┌─┬┌┐┌┌─┐  "
+echo -e " \e[92m                 ║ ║\e[95m││││  ││││├┤  \e[94m ╠═╣\e[95m├─┤│  ├┴┐│││││ ┬  "
+echo -e " \e[92m                 ╚═╝\e[95m┘└┘┴─┘┴┘└┘└─┘ \e[94m ╩ ╩\e[95m┴ ┴└─┘┴ ┴┴┘└┘└─┘ \e[32m "
 echo ""
 echo "" 
-echo -n " Paste Phishing URL here (with http or https): "
+echo -e ' \e[91m[\e[92m★\e[91m] \e[93mPaste Phishing URL here \e[96m(with http or https):\e[0m '
+echo -en "\e[32m =>\e[0m "
 read phish
 url_checker $phish
-#short=$(curl -s http://tny.im/yourls-api.php?action=shorturl\&format=simple\&url=${phish})
-	short=$(curl -s https://vurl.com/api.php?url=${phish})
-	#derek=$(curl -s https://is.gd/create.php\?format\=simple\&url=${link})
-        #checking for is.gd is working or not.
-        if [[ $derek == https://is.gd/[-0-9a-zA-Z]* ]]; then
-        shorter=${derek#https://}
-        else
-        #short=$(curl -s https://soo.gd/api.php?url=${link})
-        curl -s https://api.shrtco.de/v2/shorten?url=${link} >> log.URI 
-        grep -o 'https:[^"]*' log.URI >> bURI
-        rm log.URI
-        sed 's/\\//g' bURI >> uri.log
-        rm bURI
-        short=$(grep -o 'https://9qr.de/[-0-9a-zA-Z]*' "uri.log")
-        shorter=${short#https://}
-        fi
-        read -p $'\n\033[1;92mOnlineHacking \033[1;97m>> \033[1;37mMask Your url domain here \033[1;93m(Ex. https://facebook.com) :\e[0m ' mask
-        url_checker $mask
-	printf "${green}OnlineHacking ${white}>> ${white}Enter your key words ${yellow}: Ex. free-insta-followers\n"
-	printf "${green}OnlineHacking ${white}>> ${white}Don't use space in your words\n"
-        read -p $'\e[1;92mOnlineHacking \033[1;97m>> \e[1;37mEnter your words here :\e[0m ' words
-        final_url=$mask-$words@$shorter
-	printf "$final_url\n"
+echo ""
+echo -e " \e[95m Processing and Modifing Phishing URL ..........\e[0m"
+echo -e ""
+short=$(curl -s https://vurl.com/api.php?url=${phish})
+shorter=${short#https://}
+sleep 3
+echo ""
+echo -e ' \e[91m[\e[92m*\e[91m]\e[93m\e[1m Domain to mask the Phishing URL \e[21m\e[0m: \e[91m(Ex:\e[96m https://google.com , http://facebook.com)\e[0m'
+echo -en "\e[32m =>\e[0m "
+read mask
+url_checker $mask
+echo -e ""
+echo -e ""
+echo -e ' \e[91m[\e[92m*\e[91m] \e[93m\e[1mType social engineering words : \e[21m\e[0m\e[91m(Ex:\e[96m like, free-money, free-insta-followers)'
+echo -e " \e[32m Don't use space just use \e[91m - \e[32mbetween social engineering words\e[0m"
+echo -en "\e[32m=>\e[0m "
+read words
+echo -e ""
+echo -e "\n\e[95m Generating Masking Link by Online Hacking .......\e[0m\n"
+sleep 4
+echo ""
+final=$mask-$words@$shorter
+echo -e " \e[91m[\e[92m★\e[91m] \e[93mHere is the Masking URL:\e[97m ${final} \e[0m\n"
+echo ""
+echo ""
+echo ""
